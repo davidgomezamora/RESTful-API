deef0ee00505b5b0caa48415c6aa99a50c56a455
David Andrés Gómez Zamora <david.gomez@peoplesvoice.co>
2020-01-17 14:33:44 -0500

    feat: RESTful web API example is implemented

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # RESTful-API
-Base guide for building and settings of the RESTful API with ASP.NET Core 3.0
+Base guide for building and settings of the RESTful API with ASP.NET Core 3.0.
+
+## Evernote notes
+For more information and to read the documentation learned in the Pluralsight course, see the [link](https://www.evernote.com/client/web?login=true#?anb=true&b=61a4f299-ded9-4889-b75d-f7c27dc65fe8&n=52ff78c3-854f-4027-88e8-1579f78d7c00&s=s726&search=v4&).
+
+## Test database
+To test the RESTful API with connection to a database, [download](https://docs.microsoft.com/es-es/sql/samples/adventureworks-install-configure) the Microsoft Adventureworks test database.
+
+## Implement template
+To implement the template, it is necessary to rename the Visual Studio 2019 solution like this:
+
+  1. Open the solution
+  2. From Visual Studio 2019, rename the solution.
+  3. Close the solution
+  4. Rename the folder
+  5. Open the WebAPI project file (.csproj) with a text editor
+  6. Rename the <RootNamespace> tag to the name of the solution indicated in the second step.
---
+++ RESTful API Example/WebAPI/Startup.cs
@@ -0,0 +1,147 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Repository;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            /*
+             * ---------------------------------------------------------
+             * Configuración del contexto de la base de datos y la cadena de conexión
+             * ---------------------------------------------------------
+             */
+
+            // Establece el contexto de la base de datos y define la cadena de conexión establecida en el archivo appsettings.json
+            /*services.AddDbContext<DatabaseContext>(options => {
+                options.UseSqlServer(this.Configuration.GetConnectionString("ConnectionDatabase"));
+            });*/
+
+            /*
+             * ---------------------------------------------------------
+             * Configuración de los ambitos de la aplicación
+             * ---------------------------------------------------------
+             */
+
+            // Repositorio
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            // Contexto de la base de datos
+            // services.AddScoped(typeof(DbContext), typeof(DatabaseContext));
+
+            // Servicios de la capa ApplicationCore
+            // services.AddScoped(typeof(IBlogService), typeof(BlogService));
+
+            /*
+             * ---------------------------------------------------------
+             * Configuración del versionamiento de las web APIs/Controladores
+             * ---------------------------------------------------------
+             */
+
+            services.AddApiVersioning(options => {
+                // Cabezera HTTP, donde debe espesificarse la versión del web API a usar
+                HeaderApiVersionReader multiVersionReader = new HeaderApiVersionReader("x-version");
+                // Indica que en la petición señalamos qué versión de la API soporta la petición que hemos realizado.
+                options.ReportApiVersions = true;
+
+                // En caso de que no se notifique la versión en la petición, cómo tratamos dicha petición (si se envía un error o bien si asume la versión por defecto).
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                // Versión por defecto la API
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+
+                // Ubicación donde indicamos la versión, ya sea por QueryString o por HeaderAPIVersión
+                options.ApiVersionReader = multiVersionReader;
+            });
+
+            /*
+             * ---------------------------------------------------------
+             * Configuración del servicio de serialización de datos
+             * ---------------------------------------------------------
+             */
+
+            services.AddMvc(options => {
+                // Respeta la cabezera HTTP (Accept), donde el cliente espesifica el formato en el que requiere recibir la información por parte del controlador/web API.
+                // Si el formato solicitado no es soportado, se retorna la información en el formato definido por omición por .Net Core, el cual es JSON.
+
+                options.RespectBrowserAcceptHeader = true;
+                // Adiciona soporte al formato XML; como serialización de retorno de datos, por parte del controlador/web API
+                options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            });
+
+            // Adiciona soporte al formato XML; como serialización de entrada de datos, por parte del controlador/web API
+            services.AddMvc().AddXmlSerializerFormatters();
+
+            /*
+             * ---------------------------------------------------------
+             * Configuración del servicio de seguridad => CORS
+             * ---------------------------------------------------------
+             */
+
+            services.AddCors(options =>
+            {
+                // Cors para uso en entorno de producción
+                options.AddPolicy("Production", builder =>
+                {
+                    builder.WithOrigins("https://www.xyz.com", "https://xyz.com").AllowAnyHeader().AllowAnyMethod();
+                });
+
+                // Cors para uso en entorno de desarrollo
+                options.AddPolicy("Development", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+
+                // Usa el CORS espesificado
+                app.UseCors("Development");
+            } else
+            {
+                // Usa el CORS espesificado
+                app.UseCors("Production");
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

8666df1d8f84d1e758b50f119cdd7be36d8d5552
David Andrés Gómez Zamora <david.gomez@peoplesvoice.co>
2020-01-23 11:18:19 -0500

    feat: It is implemented base class for resource parameters

---
+++ RESTful API/Common/ResourceParameters/ResourceParameters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.ResourceParameters
+{
+    public class ResourceParameters
+    {
+        public string SearchQuery { get; set; }
+    }
+}

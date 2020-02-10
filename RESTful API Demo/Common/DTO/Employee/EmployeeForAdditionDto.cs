4bdf7132383152e38cbdcdb63a0c0d66f9033b9c
David Andrés Gómez Zamora <david.gomezamora@outlook.com>
2020-02-10 16:30:18 -0500

    feat: Input data validation is implemented

--- RESTful API Demo/Common/DTO/Employee/EmployeeForAdditionDto.cs
+++ RESTful API Demo/Common/DTO/Employee/EmployeeForAdditionDto.cs
@@ -1,15 +1,31 @@
 ﻿using Common.DTO.Order;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Common.DTO.Employee
 {
-    public class EmployeeForAdditionDto
+    public class EmployeeForAdditionDto : IValidatableObject
     {
-        public string LastName { get; set; }
+        [Required]
+        [MaxLength(10)]
         public string FirstName { get; set; }
 
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
         public List<OrderForAdditionDto> Orders { get; set; } = new List<OrderForAdditionDto>();
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (this.FirstName.Equals(this.LastName))
+            {
+                yield return new ValidationResult(
+                    "The provided first name should be different from the last name",
+                    new[] { this.GetType().Name });
+            }
+        }
     }
 }
